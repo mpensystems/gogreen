8a13bc7a6e1f28105526dc5f0d5367e8ff65282b
Sanket Sarang <sanket@blobcity.com>
2024-09-14 13:13:43 +0530

    Function for accept-booking

--- Unity/routes/v1/scm/accept-booking.js
+++ Unity/routes/v1/scm/accept-booking.js
@@ -3,35 +3,56 @@ var router = express.Router();
 var Mutex = require('async-mutex').Mutex;
 var request = require('request');
 var api = require('../../../api');
+const URLS = require('../../../urls');
 const mutex = new Mutex();
 
 router.post('/', (req, res) => {
-    let booking = req.body.booking;
+    let bid = req.body.bid;
     let riderLoc = req.body.riderLoc;
 
     mutex.acquire().then(async (release) => {
-        //TODO: fetch latest status of the booking and ensure the status is active.
+        try {
+            //Fetch the booking object from Mongo
+            let booking = await api.post(URLS.SCM_DB_FETCH, {
+                db: 'mongo',
+                table: 'Bookings',
+                q: {
+                    bid: bid
+                }
+            });
+            if (booking == null || booking.status != 'active') {
+                res.status(400).send('ER212');
+                return;
+            }
 
-        api.post(process.env.SCM_URL + '/v1/unity/get-booking', {bid: booking.bid}).then(currentBookingState => {
-            if (currentBookingState.status != 'active') res.status(400).send('ER212');
+            //Fetch the latest BookingBid object from Redis
+            let bookingBid = await api.post(URLS.SCM_DB_FETCH, {
+                db: 'redis',
+                table: 'BookingBids',
+                q: {
+                    bid: bid
+                }
+            })
+            if(bookingBid == null || bookingBid.status != 'active') {
+                res.status(400).send('ER212');
+                return;
+            }
 
             /**
              * TODO: CONVERT BOOKING TO TRIP HERE. THIS FUNCTION IS EXECUTED ONE AT A TIME AND IS THREAD SAFE
-             * 
-             * 1. Fetch Redis.BookingBids for the specified bid
-             * 2. Fetch Mongo.Bookings for the specified bid
-             * 
-             * 3. Create a new trip object
-             * 4. Insert the trip object inside Mongo.Trips
+             *
+             * 1. Create a new trip object
+             * 2. Insert the trip object inside Mongo.Trips
+             * 3. Update Booking status to trip_started
+             * 4. Update BookingBid status to trip_started
+             * 5. Send update on all BookingBids.h3is redis pub/sub channels
              */
-
-            release();
-        }).catch(err => {
-            res.status(403).send('ER'); //must send back correct status and error code
-            release();
-        })
-
-        
+            
+        } catch(err) {
+            res.status(500).send('ER500'); //must send back correct status and error code
+        }
+    }).finally(() => {
+        release();
     })
 
 })
