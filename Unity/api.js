e4546e1e9c760e753cac61870298e9c8e849e35c
Sanket Sarang <sanket@blobcity.com>
2024-09-14 13:27:25 +0530

    code cleanup

--- Unity/api.js
+++ Unity/api.js
@@ -1,22 +1,5 @@
 var request = require('request');
 
-const get = async (url, formData, headers) => new Promise(resolve => {
-    var options = {
-        'method': 'POST',
-        'url': url
-    };
-
-    if(headers != null) options.headers = headers;
-    if(formData != null) options.formData = formData;
-    
-    request(options, function (error, response) {
-        if (error) {
-            // throw new Error(error);
-        }
-        
-    });
-})
-
 const post = async (url, formData, headers) => new Promise((resolve, reject) => {
     var options = {
         'method': 'POST',
@@ -29,7 +12,7 @@ const post = async (url, formData, headers) => new Promise((resolve, reject) => 
     request(options, function (error, response) {
         if (error) {
             //TODO: Error status processing and error code pass forward is to be implemented.
-            reject('ER')
+            reject('ER500')
         } else resolve(response.body);
     });
 })
